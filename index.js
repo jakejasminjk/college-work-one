6cec3a6bfd371c1f35314a19239fa3bd05efd7bc
Kervens Jasmin <jakejasminjk@gmail.com>
2018-09-29 18:25:50 +0000

    Web scraping not wroking yet but ejs is

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ let express     = require("express"),
 //Config    
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
-let food_list = ["Mcdonalds", "Burger King"]
+let food_list = ["Mcdonald's", "BurgerKing", "Wendy's"];
 /*
 Scraping steps
 Step 1: Get and setup request, cheerio, and request-promise
@@ -20,15 +20,34 @@ Step 2: create an object that has the options(uri, promise for the reques-promis
 
 */
 
+//THE ARRAY KEEPS RETURNING UNDEFINED. MAYBE TRY ANOTHER LOOP OR SELECTOR?
+
+/*const options = {
+  uri: `https://en.wikipedia.org/wiki/List_of_fast_food_restaurant_chains`,
+  transform: function (body) {
+    return cheerio.load(body);
+  }
+};
+
+rp(options)
+  .then(($) => {
+    $('li').each(function(i, elem) {
+  food_list[i] = $(this).text();
+});
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+console.log(food_list[1]);*/
 
 //Routes
 app.get("/", (req, res) => {
-    res.render("home");
+    res.render("home", {food_list: food_list});
 });
 
 function foodStuff() {
     food_list.forEach((entry) => {
-    app.get(`/entry`, (req, res) => {
+    app.get("/" + entry, (req, res) => {
         res.render("foodPage", {food_list: food_list});
     })
 });
